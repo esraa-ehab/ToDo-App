42564100e68921b769d83af32b981b2d1f801f76
Esraa Ehab <esraa.ehab105@gmail.com>
2026-01-14 11:28:47 +0200

    update user.py

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
 import regex as re
 class User:
-    def __init__(self, id, fname, lname, email, password_hash, mobile_n, status="inactive", role="user"):
+    def __init__(self, id, fname, lname, email, password_hash, mobile, status="inactive", role="user"):
         self.id = id
         self.fname = fname,
         self.lname = lname,
         self.email = email,
         self.password_hash = password_hash,
-        self.mobile_n = mobile_n,
+        self.mobile = mobile,
         self.status = status,
         self.role = role,
 
@@ -14,19 +14,19 @@ class User:
         pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_@$])[A-Za-z\d_@$]{8,}$"
         return (re.fullmatch(pattern, self.password)) and (self.password_hash == hash_func(password))
 
-    def update_field(self, fname = None, lname = None, mobile_n = None):
+    def update_field(self, fname = None, lname = None, mobile = None):
         if fname:
             self.fname = fname
         if lname:
             self.fname = fname
-        if mobile_n:
-            self.mobile_n
+        if mobile:
+            self.mobile
 
     def user_to_dict(self):
         return {
-            "ID": self.id,
-            "first_name": self.fname,
-            "last_name": self.lname,
+            "id": self.id,
+            "fname": self.fname,
+            "lname": self.lname,
             "email": self.email,
             "password_hash": self.password_hash,
             "mobile": self.mobile_n,
